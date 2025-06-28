f02f189feaaad02feedcaa258acabc887dd955e8
Kirk Lin <linkirk@163.com>
2025-06-28 08:43:57 +0800

    chore(dev): implement hot-reloading

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,29 @@
+import type { ChildProcess } from "node:child_process";
+import { spawn } from "node:child_process";
 import { defineBuildConfig } from "unbuild";
 
+let appProcess: ChildProcess | null = null;
+
+function startApp() {
+  appProcess = spawn("node", ["dist/app.mjs"], {
+    stdio: "inherit",
+  });
+}
+
 export default defineBuildConfig({
-  entries: [
-    "src/app",
-  ],
+  entries: ["src/app"],
   declaration: true,
   clean: true,
+  hooks: {
+    "build:done": () => {
+      if (appProcess && !appProcess.killed) {
+        appProcess.once("exit", () => {
+          startApp();
+        });
+        appProcess.kill("SIGKILL");
+      } else {
+        startApp();
+      }
+    },
+  },
 });
